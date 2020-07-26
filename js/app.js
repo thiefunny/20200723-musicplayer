1d48adacd8dfcc5c66b82485887969662c63514c
Mikolaj Walanus <mikolaj@walanus.com>
2020-07-26 14:18:05 +0200

    DOM is cached

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// caching the DOM
+
+coverEl = document.querySelector(".cover__image");
+songsCounterEl = document.querySelector(".songs__counter");
+playButtonLeftEl = document.querySelector(".play__button");
+playButtonRightEl = document.querySelector(".play__button__right");
+titleAlbumEl = document.querySelector(".title__album");
+titleSongEl = document.querySelector(".title__song");
+songDuration = document.querySelector("song__duration");
+
