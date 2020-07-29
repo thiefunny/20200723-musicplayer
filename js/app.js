c906ec2cf3c70e7ef629b22ae6392a3b3e954d33
Mikolaj Walanus <mikolaj@walanus.com>
2020-07-29 18:54:43 +0200

    generating markup for playlist modules

--- js/app.js
+++ js/app.js
@@ -28,16 +28,37 @@ const songsList = [{
 const coverEl = document.querySelector(".cover__image");
 const songsCounterEl = document.querySelector(".songs__counter");
 const playButtonLeftEl = document.querySelector(".play__button");
-const playButtonRightEl = document.querySelector(".play__button__right");
 const titleAlbumEl = document.querySelector(".title__album");
 const titleSongEl = document.querySelector(".title__song");
 const songDuration = document.querySelector(".song__duration");
 const audio = document.querySelector("audio");
-
+let playModule = document.querySelector(".play__module");
 
 // Objects and methods
 
 let songId = 0;
+
+const createModule = () => {
+  let html = '';
+  playModule.innerHTML = `
+  <div class="play__module__content">
+    <div class="play__button__right">
+        &#9658; 
+        <audio src="music/morrowind.mp3"></audio>
+    </div>
+    <div class="title__area">
+      <div class="title__content">
+        <p class="title__album">${songsList[songId]["title"]}</p>
+        <p class="title__song">${songsList[songId]["artist"]}</p>
+      </div>
+    </div>
+    <div class="song__duration">${songsList[songId]["time"]}</div>
+  </div>`
+}
+
+createModule();
+
+const playButtonRightEl = document.querySelector(".play__button__right");
 
 const play = () => {
   playButtonRightEl.addEventListener("click", function () {
@@ -47,15 +68,17 @@ const play = () => {
   })
 }
 
-const renderPlaylistModule = () => {
+play();
 
-  songDuration.innerHTML = songsList[songId]["time"];
-  titleAlbumEl.innerHTML = songsList[songId]["artist"];
-  titleSongEl.innerHTML = songsList[songId]["title"];
-  play();
+// const renderPlaylistModule = () => {
 
-}
+//   songDuration.innerHTML = songsList[songId]["time"];
+//   titleAlbumEl.innerHTML = songsList[songId]["artist"];
+//   titleSongEl.innerHTML = songsList[songId]["title"];
+//   play();
+
+// }
 
 const renderPlaylist = () => {}
 
-renderPlaylistModule();
+// renderPlaylistModule();
